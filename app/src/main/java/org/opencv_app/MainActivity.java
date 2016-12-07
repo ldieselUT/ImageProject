fea54ead8b61b9dda52f7e46298f74167df5d5e6
ldiesel <ldiesel@ut.ee>
2016-12-07 19:20:22 +0200

    Decoding messages now also works

--- app/src/main/java/org/opencv_app/MainActivity.java
+++ app/src/main/java/org/opencv_app/MainActivity.java
@@ -9,6 +9,7 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.MatOfByte;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
@@ -17,8 +18,10 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.database.Cursor;
@@ -28,6 +31,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,6 +39,7 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
@@ -54,8 +59,10 @@ import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 import static org.opencv.core.CvType.CV_8U;
+import static org.opencv.core.CvType.CV_8UC3;
 import static org.opencv.core.CvType.CV_8UC4;
 
 
@@ -70,6 +77,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     private CheckBox useVisibleWatermark;
     private CheckBox useInvisibleWatermark;
     private SeekBar watermarkOpacity;
+    private String watermarkText;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -113,7 +121,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         useVisibleWatermark = (CheckBox)findViewById(R.id.visibleWatermarkBox);
         useInvisibleWatermark = (CheckBox)findViewById(R.id.invisibleWatermarkBox);
         watermarkOpacity = (SeekBar)findViewById(R.id.watermarkOpacitySeekBar);
-
+        watermarkText =  "<enter text to watermark here>";
     }
 
     @Override
@@ -201,7 +209,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         String folderName = formatter.format(today);
 
         Imgproc.cvtColor(current_frame.clone(), mIntermediateMat, Imgproc.COLOR_RGBA2BGR, 3);
-        mIntermediateMat = addWatermark(mIntermediateMat, "  <-- HEY MAN! dont steal my stuff -->  ");
+        mIntermediateMat = addWatermark(mIntermediateMat, watermarkText);
 
         File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         String filename = folderName+".png";
@@ -218,6 +226,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     }
 
     private static final int SELECT_PICTURE = 1;
+    private static final int DECODE_PICTURE = 2;
 
     public void selectWatermark(View arg0) {
         // select a file returns results to onActivityResult callback
@@ -270,6 +279,36 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                 }
             }
         }
+        if (requestCode == DECODE_PICTURE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Log.d("savePng", "got to decoded image");
+            Uri uri = data.getData();
+            String watermarkedImage = uri.toString();
+            // check if uri exists
+            if (watermarkedImage != null) {
+                try {
+                    // load image into a Bitmap
+                    InputStream is = getContentResolver().openInputStream(uri);
+                    //Bitmap bmp = BitmapFactory.decodeStream(is);
+
+                    Log.d("savePng", "loaded bitmap");
+
+                    // create a temporary Mat to make opencv operations possible
+                    Mat image = readInputStreamIntoMat(is);
+                    is.close();
+                    //Utils.bitmapToMat(bmp, image);
+                    Log.d("savePng", "made to bitmap");
+                    String result = decodeWatermark(image.clone(), 200);
+                    Log.d("savePng", "result is"+result);
+                    displayDecodeResult(result);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+
+                }
+            }
+        }
     }
 
     char[] messageArray(String message, int imgLen){
@@ -307,5 +346,150 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         Core.bitwise_and(img, clearMask, img);
         Core.bitwise_or(img, matMask, img);
         return img;
+    }
+
+    public void selectText(View view){
+        Log.d("savePng", "pressed button");
+        final EditText txtUrl = new EditText(this);
+
+        // create dialog to ask the user to enter text
+        txtUrl.setHint(watermarkText);
+        try {
+            new AlertDialog.Builder(this)
+                    .setTitle("Select Watermark text")
+                    .setMessage("Type the ext here you wish to hide in the picture!")
+                    .setView(txtUrl)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            watermarkText = txtUrl.getText().toString();
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                        }
+                    })
+                    .show();
+        }catch (Exception e){
+            Log.d("savePng", e.toString());
+        }
+    }
+
+    public void decodeImage(View view){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,
+                "Select Picture"), DECODE_PICTURE);
+    }
+
+    public String decodeWatermark(Mat img, int len){
+        //get LSBs of image
+        char[] binary = getText(img);
+        Log.d("savePng", "made it through get text");
+        //convert to ascii numbers
+        for (int i = 0; i < binary.length; i++) {
+            binary[i] &= 0x1;
+            binary[i] += 0x30;
+        }
+        Log.d("savePng", "made it through binary");
+        //return decrypted string
+        return decryptBinary(binary, len);
+    }
+
+    public char[] getText(Mat img){
+        // mask out LSBs of image
+        Mat mask = new Mat(img.rows(), img.cols(), img.type(), new Scalar(0x1,0x1,0x1));
+        Core.bitwise_and(img, mask, img);
+        // convert Mat to byte array
+        byte[] return_buff = new byte[(int) (img.total() * img.channels())];
+        img.get(0, 0, return_buff);
+        // convert byte array to char array
+        String returnString = new String(return_buff);
+        char[] returnArray = returnString.toCharArray();
+
+        return returnArray;
+    }
+
+    public String decryptBinary(char[] message, int len){
+        String binString = new String(message);
+        String returnString = "";
+
+        //split into strings of len 7
+        Pattern p = Pattern.compile("(.{7})");
+
+        String[] chars = splitStringEvery(binString,7);
+        //String[] chars = binString.substring(0,len).split("(?<=\\G.{7})");
+        Log.d("savePng", String.format("split len %d", chars.length));
+        for (int i =0; i< len; i++){
+            //convert 7 bit code to ascii characters
+            try {
+                byte chr = Byte.parseByte(chars[i], 2);
+                returnString += (char) chr;
+            }catch (Exception e) {
+                Log.d("savePng", String.format("%d %s", i, returnString));
+                break;
+            }
+        }
+        Log.d("savePng", "return string\n"+returnString);
+        return returnString;
+    }
+
+    public void displayDecodeResult(String result){
+        try {
+            new AlertDialog.Builder(this)
+                    .setTitle("The decoded message is:")
+                    .setMessage(result)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                        }
+                    })
+                    .show();
+        }catch (Exception e){
+            Log.d("savePng", e.toString());
+        }
+    }
+
+    // borrowed from here http://stackoverflow.com/questions/12295711/split-a-string-at-every-nth-position
+    public String[] splitStringEvery(String s, int interval) {
+        int arrayLength = (int) Math.ceil(((s.length() / (double)interval)));
+        String[] result = new String[arrayLength];
+
+        int j = 0;
+        int lastIndex = result.length - 1;
+        for (int i = 0; i < lastIndex; i++) {
+            result[i] = s.substring(j, j + interval);
+            j += interval;
+        } //Add the last bit
+        result[lastIndex] = s.substring(j);
+
+        return result;
+    }
+
+    //borrowed from here http://answers.opencv.org/question/31855/java-api-loading-image-from-any-java-inputstream/
+    private static Mat readInputStreamIntoMat(InputStream inputStream) throws IOException {
+        // Read into byte-array
+        byte[] temporaryImageInMemory = readStream(inputStream);
+
+        // Decode into mat. Use any IMREAD_ option that describes your image appropriately
+        Mat outputImage = Imgcodecs.imdecode(new MatOfByte(temporaryImageInMemory), Imgcodecs.IMREAD_COLOR);
+
+        return outputImage;
+    }
+
+    private static byte[] readStream(InputStream stream) throws IOException {
+        // Copy content of the image to byte-array
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[16384];
+
+        while ((nRead = stream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+
+        buffer.flush();
+        byte[] temporaryImageInMemory = buffer.toByteArray();
+        buffer.close();
+        stream.close();
+        return temporaryImageInMemory;
     }
 }
